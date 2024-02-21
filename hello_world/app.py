6122f6500c57bdbc325bd4975b58e5c9234e20cb
Cynthia Zhang <xintian0122@gmail.com>
2024-02-21 13:42:18 -0500

    to test CI/CD pipeline

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world Feb 21! First try after complete CI/CD pipeline ",
             # "location": ip.text.replace("\n", "")
         }),
     }
